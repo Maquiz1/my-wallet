803139635003ee4f823729185eba24bd0461dba4
Maquiz1 <manquiz92@gmail.com>
2025-08-25 10:47:14 +0300

    fix all

--- backend/mentorship/models.py
+++ backend/mentorship/models.py
@@ -17,6 +17,13 @@ GRADE_CHOICES = [
     ('Poor', 'Poor'),
 ]
 
+class Status(models.Model):
+    name = models.CharField(max_length=50, unique=True)  # e.g., Assigned, Completed, Reviewed
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Mentorship(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
