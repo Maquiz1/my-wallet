33ef30777756390905a380d4876d9e6cce28b945
Maquiz1 <manquiz92@gmail.com>
2025-08-26 20:31:22 +0300

    fix all

---
+++ backend/staticfiles/mentorship/js/visit_form_validation.js
@@ -0,0 +1,15 @@
+// static/mentorship/js/visit_form_validation.js
+
+(() => {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
