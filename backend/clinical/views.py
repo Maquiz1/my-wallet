76adfe4fe22f093b87971429efcd185a383f8a46
Maquiz1 <manquiz92@gmail.com>
2025-08-26 11:17:39 +0300

    fix all

--- backend/clinical/views.py
+++ backend/clinical/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
-from .models import Disease, Competence
 from django.db.models import Count
+from .models import Disease, Competence
 
 class DiseaseListView(ListView):
     model = Disease
@@ -12,12 +12,28 @@ class DiseaseListView(ListView):
             competence_count=Count("competences", distinct=True)
         )
 
+
 class DiseaseDetailView(DetailView):
     model = Disease
     template_name = 'clinical/disease_detail.html'
     context_object_name = 'disease'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add competences with assessment counts
+        context["competences"] = self.object.competences.annotate(
+            assessment_count=Count("assignedcompetence", distinct=True)
+        )
+        return context
+
+
 class CompetenceDetailView(DetailView):
     model = Competence
     template_name = 'clinical/competence_detail.html'
     context_object_name = 'competence'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add assessment count
+        context["assessment_count"] = self.object.assignedcompetence_set.count()
+        return context
