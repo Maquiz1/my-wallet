6799bf93536a3fb0566a43a41cecce95b26be34b
Maquiz1 <manquiz92@gmail.com>
2025-08-28 02:11:38 +0300

    fix all

--- backend/reports/views/general_visualization_view.py
+++ backend/reports/views/general_visualization_view.py
@@ -1,76 +1,98 @@
+from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import render
 from django.db.models import Count, Avg
 from mentorship.models import AssignedCompetence, Visit
-from locations.models import Site, Country
+from locations.models import Site
 from clinical.models import Disease
 from django.contrib.auth import get_user_model
+from django.core.serializers.json import DjangoJSONEncoder
 
 User = get_user_model()
+
 
 class GeneralVisualizationReportView(View):
     template_name = "reports/general_visualization_report.html"
 
     def get(self, request, *args, **kwargs):
-        # ----- Competence Data -----
-        disease_id = request.GET.get("disease")
-        user_filter = request.GET.get("user_filter")  # mentor, mentee, all
+        # --- Filters ---
+        disease_id = request.GET.get("disease", "all")
+        user_filter = request.GET.get("user_filter", "all")
+        site_id = request.GET.get("site", "all")
+        mentor_id = request.GET.get("mentor", "all")
+        mentee_id = request.GET.get("mentee", "all")
+
+        # --- Competence per Disease ---
         comp_qs = AssignedCompetence.objects.all()
-
-        if disease_id and disease_id != "all":
+        if disease_id != "all":
             comp_qs = comp_qs.filter(competence__disease_id=disease_id)
+
         if user_filter == "mentor":
-            comp_qs = comp_qs.filter(assigned_by__groups__name="Mentor")
+            comp_per_disease = comp_qs.values("competence__disease__name").annotate(avg_grade=Avg("mentor_grade"))
         elif user_filter == "mentee":
-            comp_qs = comp_qs.filter(mentee__groups__name="Mentee")
+            comp_per_disease = comp_qs.values("competence__disease__name").annotate(avg_grade=Avg("mentee_grade"))
+        else:
+            comp_per_disease = comp_qs.values("competence__disease__name").annotate(
+                avg_mentor=Avg("mentor_grade"),
+                avg_mentee=Avg("mentee_grade"),
+            )
 
-        competence_per_disease = comp_qs.values("competence__disease__name").annotate(avg_grade=Avg("mentee_grade"))
-
-        # ----- Users Data -----
+        # --- Users Data ---
         users_data = []
-        for user in User.objects.all():
-            groups = user.groups.values_list("name", flat=True)
-            mentee_count = AssignedCompetence.objects.filter(mentee=user).count()
-            mentor_count = AssignedCompetence.objects.filter(assigned_by=user).count()
-            avg_mentee_grade = AssignedCompetence.objects.filter(mentee=user).aggregate(avg=Avg("mentee_grade"))["avg"]
-            avg_mentor_grade = AssignedCompetence.objects.filter(assigned_by=user).aggregate(avg=Avg("mentor_grade"))["avg"]
-            visits_conducted = Visit.objects.filter(mentor=user).count()
-            visits_attended = Visit.objects.filter(attendees=user).count() if hasattr(Visit, "attendees") else 0
+        for user in User.objects.filter(groups__name__in=["Mentor", "Mentee"]).distinct():
+            mentee_qs = AssignedCompetence.objects.filter(mentee=user)
+            mentor_qs = AssignedCompetence.objects.filter(assigned_by=user)
             users_data.append({
                 "username": user.username,
-                "groups": list(groups),
-                "mentee_count": mentee_count,
-                "mentor_count": mentor_count,
-                "avg_mentee_grade": avg_mentee_grade,
-                "avg_mentor_grade": avg_mentor_grade,
-                "visits_conducted": visits_conducted,
-                "visits_attended": visits_attended,
+                "avg_mentor_grade": float(mentor_qs.aggregate(avg=Avg("mentor_grade"))["avg"] or 0),
+                "avg_mentee_grade": float(mentee_qs.aggregate(avg=Avg("mentee_grade"))["avg"] or 0),
             })
 
-        # ----- Visits Data -----
-        start_date = request.GET.get("start_date")
-        end_date = request.GET.get("end_date")
-        visits_qs = Visit.objects.all()
-        if start_date and end_date:
-            visits_qs = visits_qs.filter(start_date__range=[start_date, end_date])
+        # --- Visits per Site ---
+        visits_per_site = list(Visit.objects.values("site__name").annotate(total=Count("id")))
 
-        visits_per_site = visits_qs.values("site__name").annotate(total=Count("id"))
+        # --- Sites per Country ---
+        sites_per_country = list(Visit.objects.values("site__district__region__country__name").annotate(total_sites=Count("site")))
 
-        # ----- Locations Data -----
-        sites_per_country = Site.objects.values(
-            "district__region__country__name"
-        ).annotate(total_sites=Count("id"))
-        visits_per_location = Visit.objects.values("site__name").annotate(total_visits=Count("id"))
+        # --- Visits per Location ---
+        visits_per_location = list(Visit.objects.values("site__name").annotate(total_visits=Count("id")))
 
+        # --- Top 5 Competences ---
+        top_comp_qs = AssignedCompetence.objects.all()
+        if disease_id != "all":
+            top_comp_qs = top_comp_qs.filter(competence__disease_id=disease_id)
+        if site_id != "all":
+            top_comp_qs = top_comp_qs.filter(visit_day__visit__site_id=site_id)
+        if mentor_id != "all":
+            top_comp_qs = top_comp_qs.filter(assigned_by_id=mentor_id)
+        if mentee_id != "all":
+            top_comp_qs = top_comp_qs.filter(mentee_id=mentee_id)
+
+        top_competences = list(top_comp_qs.values(
+            "competence__name", "competence__disease__name"
+        ).annotate(total_assigned=Count("id")).order_by("-total_assigned")[:5])
+
+        # --- AJAX JSON response ---
+        if request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return JsonResponse({
+                "competence_per_disease": list(comp_per_disease),
+                "users_data": users_data,
+                "visits_per_site": visits_per_site,
+                "sites_per_country": sites_per_country,
+                "visits_per_location": visits_per_location,
+                "top_competences": top_competences,
+            }, encoder=DjangoJSONEncoder)
+
+        # --- Render template ---
         context = {
-            "competence_per_disease": list(competence_per_disease),
-            "users_data": users_data,
-            "visits_per_site": list(visits_per_site),
-            "sites_per_country": list(sites_per_country),
-            "visits_per_location": list(visits_per_location),
             "diseases": Disease.objects.all(),
-            "selected_disease": int(disease_id) if disease_id and disease_id != "all" else None,
-            "user_filter": user_filter or "all",
+            "mentors": User.objects.filter(groups__name="Mentor"),
+            "mentees": User.objects.filter(groups__name="Mentee"),
+            "sites": Site.objects.all(),
+            "selected_disease": int(disease_id) if disease_id != "all" else None,
+            "selected_site": int(site_id) if site_id != "all" else None,
+            "selected_mentor": int(mentor_id) if mentor_id != "all" else None,
+            "selected_mentee": int(mentee_id) if mentee_id != "all" else None,
+            "user_filter": user_filter,
         }
         return render(request, self.template_name, context)
-
